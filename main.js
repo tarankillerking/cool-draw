c0058dffe27309d5a9dfe23c3c2f3cb2091d9fd6
tarankillerking <79645217+tarankillerking@users.noreply.github.com>
2022-05-24 19:33:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 array_1=["pen","paper","book","bottle"]
 random_no = Math.floor((Math.random()*array_1.length)+1)
-sketch=array_1[randnom_no]
+sketch=array_1[random_no]
 document.getElementById("sketchname").innerHTML="sketch to be drawn "+sketch
 score=0
 drawnsketch=""
